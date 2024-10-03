40898de5673802e4a51a88c65e11d03e205ad7ab
Syed-mansoor <saeedmansoor56@gmail.com>
2024-10-03 15:48:50 +0530

    setup file added

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-from setup import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name="phishing classifier",
+    version="0.0.0",
+    author="syed mansoor",
+    author_email="saeedmansoor56@gmail.com",
+    packages=find_packages()
+)
